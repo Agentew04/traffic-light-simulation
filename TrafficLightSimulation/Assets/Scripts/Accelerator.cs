6afa22451d886d045af3f1e626c4620ba32d7731
Antônio Amadeu Dall'Agnol Rohr <aarohr@inf.ufsm.br>
2024-11-18 23:44:13 -0300

    mais mudancas

--- TrafficLightSimulation/Assets/Scripts/Accelerator.cs
+++ TrafficLightSimulation/Assets/Scripts/Accelerator.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Accelerator : MonoBehaviour
 {
     public float accelerationBoost = 450f;  // A aceleração desejada quando o carro entra na zona
+    public float accelerationBoostGol = -450f;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,7 +14,16 @@ public class Accelerator : MonoBehaviour
             {
                 // Chama o método SetAcceleration para mudar a aceleração para 400
                 carController.SetAcceleration(accelerationBoost);
-                Debug.Log("Aceleração aumentada para 450.");
+                Debug.Log("Aceleração audi aumentada para 450.");
+            }
+        }
+        if (other.CompareTag("Gol"))
+        {
+            if (other.TryGetComponent<CarController>(out var carController))
+            {
+                // Chama o método SetAcceleration para mudar a aceleração para 400
+                carController.SetAcceleration(accelerationBoostGol);
+                Debug.Log("Aceleração gol aumentada para 450.");
             }
         }
     }
